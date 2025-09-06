423bace7b74d08e77bf8e4d57589697ef62b4f38
Mercedes Custodio <52111388+frontendmercc@users.noreply.github.com>
2025-09-06 18:02:52 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const yearSpan = document.createElement('span');
-  yearSpan.textContent = new Date().getFullYear();
-  document.querySelector('footer p').append(" " + yearSpan.textContent);
-});
+function toggleTranslate() {
+  if (!window.googleTranslateInitialized) {
+    const gtScript = document.createElement('script');
+    gtScript.src = "//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit";
+    document.body.appendChild(gtScript);
+    window.googleTranslateInitialized = true;
+  } else {
+    document.getElementById('google_translate_element').style.display = 'block';
+  }
+}
+
+function googleTranslateElementInit() {
+  new google.translate.TranslateElement({
+    pageLanguage: 'en',
+    includedLanguages: 'es,pt,fr,en',
+    layout: google.translate.TranslateElement.InlineLayout.SIMPLE
+  }, 'google_translate_element');
+}
